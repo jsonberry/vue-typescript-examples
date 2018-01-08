2ab292ac31a03eb65642fd61490c89cc64811832
jsonberry <jason.s.awbrey@gmail.com>
2018-01-08 07:42:26 -0800

    add notes for the custom form input

--- src/components/forms/CustomFormInput.vue
+++ src/components/forms/CustomFormInput.vue
@@ -3,8 +3,8 @@
                 <input
                         type="text"
                         :placeholder="placeholder"
-                        :value="entry"
-                        @change="changed"
+                        :value="foo"
+                        @change="bar"
                 >
         </div>
 </template>
@@ -15,10 +15,29 @@ import { Vue, Component, Prop, Model } from '@/vue-script'
 @Component({})
 export default class FormTextInput extends Vue {
         @Prop() placeholder: string
-        @Model('changed') entry: string
 
-        changed(ev) {
-                this.$emit('changed', ev.target.value)
+        /**
+         *      Under the hood this is using v-model
+         *      The connection to the data model is assigned as "foo"
+         *      The event emitted "changed" allows for a proper binding
+         *      To the parent where the form model
+         *
+         *      By default the property would be "value" and the
+         *      necessary event to emit would be "input"
+         *
+         *      Using @Model allows us access to the Vue instance "model"
+         *      which lets us customize the name of the event to emit, and
+         *      the name of the property
+         *
+         *      Notice that in the parent component we don't have to listen
+         *      for the "bar" event, and we don't have to pass a specific "foo" property
+         *      The binding is being managed for us by @Model
+         */
+        @Model('bar') foo: string
+
+        // $event is passed implicitly from the @changed directive
+        bar(ev) {
+                this.$emit('bar', ev.target.value)
         }
 }
 </script>
